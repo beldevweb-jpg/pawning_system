36f53161b21719ea47f4cdc182f17cd4c6237531
Urtopia <thanabut.patt@gmail.com>
2026-01-26 00:43:08 +0700

    update front end 2.0

--- Modules/Commerce/resources/views/Commerce/crate_pawming.blade.php
+++ Modules/Commerce/resources/views/Commerce/crate_pawming.blade.php
@@ -7,158 +7,90 @@
   <script src="app.js" defer></script> 
 </head>
 <body>
-    
-  <header class="header">
-    <div class="user">
-      <span class="avatar">👤</span>
-      <span>username</span>
-    </div>
-    <button class="logout">ออกจากระบบ</button>
-  </header>
 
-  <h1 class="title">ขาย</h1>
+<header class="header">
+  <div class="user">
+    <span class="avatar">👤</span>
+    <span>username</span>
+  </div>
+  <button class="logout">ออกจากระบบ</button>
+</header>
+
+<h1 class="title">ขาย</h1>
+
+<div class="container">
 
   <div class="card">
     <h2>ประเภท</h2>
 
-    <label class="option">
-      <input type="radio" name="selltype" value="pawn">
-      วาง
-    </label>
+    <label class="option"><input type="radio" name="selltype"> วาง</label>
+    <label class="option"><input type="radio" name="selltype"> ต่อ</label>
+    <label class="option"><input type="radio" name="selltype"> ไถ่</label>
+    <label class="option"><input type="radio" id="pawn-other" name="selltype"> อื่นๆ</label>
 
-    <label class="option">
-      <input type="radio" name="selltype" value="continue">
-      ต่อ
-    </label>
+    <input class="input" id="typesell-other-input" placeholder="กรอกประเภท" disabled>
+  </div>
 
-    <label class="option">
-      <input type="radio" name="selltype" value="Redeem">
-      ไถ่
-    </label>
-
-    <label class="option">
-      <input type="radio" name="selltype" value="other" id="pawn-other">
-      อื่นๆ
-    </label>
-
-    <input
-      type="text"
-      id="typesell-other-input"
-      class="input"
-      placeholder="กรอกประเภทสินค้า"
-      disabled
-    >
-    </div>  
-
-  <!-- CUSTOMER FORM -->  
   <div class="card">
     <h2 class="section-title">ข้อมูลลูกค้า</h2>
 
     <div class="form-row">
       <label>หมายเลขเครื่อง</label>
-      <input type="text" name="serial-number" placeholder="กรอกข้อมูล">
+      <input>
     </div>
 
     <div class="form-row">
       <label>ชื่อ-นามสกุล</label>
-      <input type="text" name="name" placeholder="กรอกข้อมูล">
+      <input>
     </div>
 
     <div class="form-row">
       <label>บัตรประชาชน</label>
-      <input type="text"  name="tax-number" placeholder="กรอกข้อมูล">
+      <input>
     </div>
 
     <div class="form-row">
       <label>เบอร์ติดต่อ</label>
-      <input type="text" name="phone" placeholder="กรอกข้อมูล">
+      <input>
     </div>
   </div>
 
-<div class="container">
-
-  <!-- ประเภทสินค้า -->
   <div class="card">
     <h2>ประเภทสินค้า</h2>
 
-    <label class="option">
-      <input type="radio" name="product_type" value="mobile">
-      มือถือ
-    </label>
+    <label class="option"><input type="radio" name="product_type"> มือถือ</label>
+    <label class="option"><input type="radio" name="product_type"> Tablet</label>
+    <label class="option"><input type="radio" id="type-other" name="product_type"> อื่นๆ</label>
 
-    <label class="option">
-      <input type="radio" name="product_type" value="tablet">
-      Tablet
-    </label>
-
-    <label class="option">
-      <input type="radio" name="product_type" value="other" id="type-other">
-      อื่นๆ
-    </label>
-
-    <input
-      type="text"
-      id="type-other-input"
-      class="input"
-      placeholder="กรอกประเภทสินค้า"
-      disabled
-    >
+    <input class="input" id="type-other-input" placeholder="กรอกประเภทสินค้า" disabled>
   </div>
 
-  <!-- ยี่ห้อ -->
-  <div class="card">
-    <h2>ยี่ห้อ</h2>
+ <div class="card">
+  <h2>ยี่ห้อ</h2>
 
-    <label class="option">
-      <input type="radio" name="brand" value="iphone">
-      iPhone
-    </label>
+  <div class="option-grid">
+    <label class="option"><input type="radio" name="brand" value="iphone"> iPhone</label>
+    <label class="option"><input type="radio" name="brand" value="Samsung"> Samsung</label>
+    <label class="option"><input type="radio" name="brand" value="oppo"> OPPO</label>
 
-    <label class="option">
-      <input type="radio" name="brand" value="samsung">
-      Samsung
-    </label>
+    <label class="option"><input type="radio" name="brand" value="huawei"> Huawei</label>
+    <label class="option"><input type="radio" name="brand" value="realme"> realme</label>
+    <label class="option"><input type="radio" name="brand" value="vivo"> vivo</label>
 
+    <div class="option-other">
     <label class="option">
-      <input type="radio" name="brand" value="oppo">
-      OPPO
-    </label>
-
-    <label class="option">
-      <input type="radio" name="brand" value="huawei">
-      huawei
-    </label>
-
-    <label class="option">
-      <input type="radio" name="brand" value="realme">
-      realme
-    </label>
-
-    <label class="option">
-      <input type="radio" name="brand" value="vivo">
-      vivo
-    </label>
-
-    <label class="option">
-      <input type="radio" name="brand" value="other" id="brand-other">
+      <input type="radio" id="brand-other" name="brand">
       อื่นๆ
     </label>
-
-    <input  
-      type="text"
-      id="brand-other-input"
-      class="input"
-      placeholder="กรอกยี่ห้อ"
-      disabled
-    >
+    <input class="input" id="brand-other-input" placeholder="กรอกยี่ห้อ" disabled>
   </div>
+</div>
 
   <div class="submit">
-    <button name="cancel" type="button" onclick="goPage('index.html')">ยกเลิก</button>
-    <button name="next" type="button" onclick="goPage('report.html')">ดำเนินการต่อ</button>
+    <button name="cancel" onclick="goPage('index.html')">ยกเลิก</button>
+    <button name="next">ดำเนินการต่อ</button>
   </div>
 
 </div>
-
 </body>
 </html>
--- Modules/Commerce/resources/views/Commerce/crate_type_of_sale.blade.php
+++ Modules/Commerce/resources/views/Commerce/crate_type_of_sale.blade.php
@@ -9,22 +9,25 @@
     
 </head>
 <body>
-    <header class="header">
-    <div class="user-info">
-      <div class="avatar"></div>
-      <span class="username">username</span>
-    </div>
-    <button class="logout">ออกจากระบบ</button>
-  </header>
 
-  <main class="container">
-    <h1 class="title">โอ๋ โมบาย</h1>
+<header class="header">
+  <div class="user-info">
+    <div class="avatar"></div>
+    <span class="avatar">👤</span>
+    <span class="username">username</span>
+  </div>
+  <button class="logout">ออกจากระบบ</button>
+</header>
 
-    <section class="menu-card">
-      <div class="menu-item" onclick="goPage('pawning.html')" name="pawning" type="button" >ขาย</div>
-      <div class="menu-item" name="sale" type="button" >ขายหน้าร้าน</div>
-      <div class="menu-item" name="service" type="button">บริการ</div>
-    </section>
-  </main>
+<main class="container">
+  <h1 class="title">โอ๋ โมบาย</h1>
+
+  <section class="menu-card">
+    <div class="menu-item" onclick="goPage('pawning.html')">ขาย</div>
+    <div class="menu-item">ขายหน้าร้าน</div>
+    <div class="menu-item">บริการ</div>
+  </section>
+</main>
+
 </body>
 </html>
