a19695f5e1d902e07d93624f576fd6f0fe615655
Urtopia <thanabut.patt@gmail.com>
2026-01-28 22:09:59 +0700

    restore file

--- Modules/Commerce/resources/views/Commerce/create_pawning.blade.php
+++ Modules/Commerce/resources/views/Commerce/create_pawning.blade.php
@@ -8,154 +8,92 @@
     <link rel="stylesheet" href="{{ asset('css/error.css') }}">
 </head>
 
-<body>
+    <body>
 
-    <header class="header">
-        <div class="user">
-            <span class="avatar">👤</span>
-            <span>username</span>
-        </div>
-        <button class="logout">ออกจากระบบ</button>
-    </header>
+        <header class="header">
+            <div class="user">
+                <span class="avatar">👤</span>
+                <span>username</span>
+            </div>
+            <button class="logout">ออกจากระบบ</button>
+        </header>
 
-    @if (session()->has('success'))
-        <div class="alert alert-info" role="alert">
-            <strong>{{ session()->get('success') }}</strong>
-        </div>
-        @php session()->forget('success') @endphp
-    @endif
-
-    @if ($errors->any())
-        <div class="alert alert-danger" role="alert">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </div>
-    @endif
-
-    <h1 class="title">ขาย</h1>
-
-    <form method="POST" action="{{ route('commerce.store_pawning', $id) }}">
-        @csrf
+        <h1 class="title">ขาย</h1>
 
         <div class="container">
 
-            <!-- ประเภทการขาย -->
             <div class="card">
                 <h2>ประเภท</h2>
 
-                <label class="option">
-                    <input type="radio" name="selltype" value="pawn"
-                        {{ old('type_serve') == 'pawn' ? 'checked' : '' }} required> วาง
-                </label>
-                <label class="option">
-                    <input type="radio" name="selltype" value="extend"
-                        {{ old('type_serve') == 'extend' ? 'checked' : '' }}> ต่อ
-                </label>
-                <label class="option">
-                    <input type="radio" name="selltype" value="redeem"
-                        {{ old('type_serve') == 'redeem' ? 'checked' : '' }}> ไถ่
-                </label>
-                <label class="option">
-                    <input type="radio" name="selltype" value="other" id="pawn-other"
-                        {{ old('type_serve') == 'other' ? 'checked' : '' }}> อื่นๆ
-                </label>
+                <label class="option"><input type="radio" name="selltype"> วาง</label>
+                <label class="option"><input type="radio" name="selltype"> ต่อ</label>
+                <label class="option"><input type="radio" name="selltype"> ไถ่</label>
+                <label class="option"><input type="radio" id="pawn-other" name="selltype"> อื่นๆ</label>
 
-                <input class="input" name="others" id="type-serve-input" placeholder="กรอกประเภท"
-                    value="{{ old('others') }}" {{ old('type_serve') == 'other' ? '' : 'disabled' }}>
+                <input class="input" id="typesell-other-input" placeholder="กรอกประเภท" disabled>
             </div>
 
-            <!-- ข้อมูลลูกค้า -->
             <div class="card">
-                <h2>ข้อมูลลูกค้า</h2>
+                <h2 class="section-title">ข้อมูลลูกค้า</h2>
+
+                <div class="form-row">
+                    <label>หมายเลขเครื่อง</label>
+                    <input>
+                </div>
 
                 <div class="form-row">
                     <label>ชื่อ-นามสกุล</label>
-                    <input name="fullname" type="text" value="{{ old('fullname') }}">
+                    <input>
                 </div>
 
                 <div class="form-row">
                     <label>บัตรประชาชน</label>
-                    <input name="tax_number" type="number" value="{{ old('tax_number') }}">
+                    <input>
                 </div>
 
                 <div class="form-row">
                     <label>เบอร์ติดต่อ</label>
-                    <input name="phone" type="number" value="{{ old('phone') }}">
+                    <input>
                 </div>
-
-                <div class="form-row">
-                    <label>สถานะ</label>
-                </div>
+                <h5>สถานะ</h5>
             </div>
 
-            <!-- ประเภทสินค้า -->
             <div class="card">
                 <h2>ประเภทสินค้า</h2>
 
-                <label class="option">
-                    <input type="radio" name="type_category" value="mobile"
-                        {{ old('type_category') == 'mobile' ? 'checked' : '' }} required> มือถือ
-                </label>
-                <label class="option">
-                    <input type="radio" name="type_category" value="tablet"
-                        {{ old('type_category') == 'tablet' ? 'checked' : '' }}> Tablet
-                </label>
-                <label class="option">
-                    <input type="radio" name="type_category" value="other" id="type-other"
-                        {{ old('type_category') == 'other' ? 'checked' : '' }}> อื่นๆ
-                </label>
+                <label class="option"><input type="radio" name="product_type"> มือถือ</label>
+                <label class="option"><input type="radio" name="product_type"> Tablet</label>
+                <label class="option"><input type="radio" id="type-other" name="product_type"> อื่นๆ</label>
 
-                <input class="input" name="other" id="type-other-input" placeholder="กรอกประเภทสินค้า"
-                    value="{{ old('description') }}" {{ old('type_category') == 'other' ? '' : 'disabled' }}>
+                <input class="input" id="type-other-input" placeholder="กรอกประเภทสินค้า" disabled>
             </div>
 
-            <!-- ยี่ห้อ -->
             <div class="card">
                 <h2>ยี่ห้อ</h2>
 
-                @foreach (['iphone', 'samsung', 'oppo', 'vivo', 'huawei', 'realme',] as $brand)
-                    <label class="option">
-                        <input type="radio" name="brand" value="{{ $brand }}"
-                            {{ old('brand') == $brand ? 'checked' : '' }} required>
-                        {{ ucfirst($brand) }}
-                    </label>
-                @endforeach
-                <label class="option">
-                    <input type="radio" name="brand" value="other" id="brand-other"
-                        {{ old('type_category') == 'other' ? 'checked' : '' }}> อื่นๆ
-                </label>
-                <input class="input" name="other" id="brand-other-input" placeholder="กรอกยี่ห้อ"
-                    value="{{ old('description') }}" {{ old('type_category') == 'other' ? '' : 'disabled' }}>
+                <div class="option-grid">
+                    <label class="option"><input type="radio" name="brand" value="iphone"> iPhone</label>
+                    <label class="option"><input type="radio" name="brand" value="Samsung"> Samsung</label>
+                    <label class="option"><input type="radio" name="brand" value="oppo"> OPPO</label>
+
+                    <label class="option"><input type="radio" name="brand" value="huawei"> Huawei</label>
+                    <label class="option"><input type="radio" name="brand" value="realme"> realme</label>
+                    <label class="option"><input type="radio" name="brand" value="vivo"> vivo</label>
+
+                    <div class="option-other">
+                        <label class="option">
+        <input type="radio" id="brand-other" name="brand">
+        อื่นๆ
+        </label>
+                        <input class="input" id="brand-other-input" placeholder="กรอกยี่ห้อ" disabled>
+                    </div>
+                </div>
+
+                <div class="submit">
+                    <button name="cancel" onclick="goPage('index.html')">ยกเลิก</button>
+                    <button name="next">ดำเนินการต่อ</button>
+                </div>
+                
             </div>
-
-            <!-- รุ่น -->
-            <div class="card">
-                <h2>รุ่น</h2>
-                <input class="input" name="model" type="text" value="{{ old('model') }}" required>
-                <h2>รหัส IMEI</h2>
-                <input class="input" name="imei" type="text" value="{{ old('imei') }}" required>
-            </div>
-            {{-- ราคา --}}
-            <div class="card">
-                <h2>ราคา</h2>
-                <input class="input" name="price" type="number" value="{{ old('price') }}" required>
-                <h2>หมายเหตุ</h2>
-                <input class="input" name="note" type="text" value="{{ old('note') }}">
-            </div>
-            <div class="submit">
-                <button type="button" onclick="window.location.href='{{ '' }}'">
-                    ยกเลิก
-                </button>
-
-                <button type="submit" id="btn-submit">บันทึก</button>
-
-            </div>
-        </div>
-    </form>
-
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-</body>
 
 </html>
